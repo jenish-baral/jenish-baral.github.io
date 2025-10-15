945a3592c2321823fa994a9d1ca242753242b74b
sumit poudel <sumitpoudel325@gmail.com>
2025-10-15 19:33:15 +0545

    jenish baral
    initiating jenish baral protfolioJ

---
+++ README.md
@@ -0,0 +1,11 @@
+# portfolio 
+
+hosted on cloudflare using pages [look👀](https://sumit-poudel.com.np/)
+
+## iterrations 
+
+- current iterration
+    ![Visual](./assets/current.png)
+
+- previous iterration
+    ![Visual](./assets/first.png)
---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+import { defineConfig, fontProviders } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://www.sumit-poudel.com.np',
+  trailingSlash: "never",
+   output: 'static',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [react(), sitemap()],
+
+  experimental: {
+    fonts: [{
+      provider: fontProviders.google(),
+      name: "Geist",
+      cssVariable: "--font-geist",
+      fallbacks: ["Inter", "sans-serif"],
+    }]
+  },
+});
